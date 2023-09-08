22d36f9ab0b2f8cc1fa7ee8c73bca4eed7173676
muskan gupta <101249185+muskangupta2002@users.noreply.github.com>
2023-09-09 01:27:41 +0530

    questions and timer function updated

--- QuizMaster/QuizMaster.c
+++ QuizMaster/QuizMaster.c
@@ -16,11 +16,11 @@ typedef struct {
 } User;
 
 Question questions[5] = {
-    {"Question 1: What is this", {"Option A", "Option B", "Option C", "Option D"}, 1},
-    {"Question 2: What is this", {"Option A", "Option B", "Option C", "Option D"}, 1},
-    {"Question 3: What is this", {"Option A", "Option B", "Option C", "Option D"}, 1},
-    {"Question 4: What is this", {"Option A", "Option B", "Option C", "Option D"}, 1},
-    {"Question 5: What is this", {"Option A", "Option B", "Option C", "Option D"}, 1}
+    {"Question 1: Who is the father of C language?", {"Steve Jobs", "James Gosling", "Dennis Ritchie", "Rasmus Lerdorf"}, 3},
+    {"Question 2: Which of the following is not a valid C variable name?", {"int number;", "float rate;", "int variable_count;", "int $main;"}, 4},
+    {"Question 3: Which is valid C expression?", {"int my_num = 100,000;", "int my_num = 100000;", "int my num = 1000;", "int $my_num = 10000;"}, 2},
+    {"Question 4: Which of the following cannot be a variable name in C?", {"volatile", "true", "friend", "export"}, 2},
+    {"Question 5: Which keyword is used to prevent any changes in the variable within a C program?", {"immutable", "mutable", "const", "volatile"}, 3}
     
 };
 
@@ -47,35 +47,45 @@ int main() {
     user.marks = 0;
     int currentQuestion = 0; // Initialize the question index
     time_t startTime = time(NULL);
-    time_t endTime = startTime + 1 * 60; // 30 minutes time limit
+    time_t endTime = startTime + 1 * 60; // 1 minute time limit
 
     while (currentQuestion < 5 && time(NULL) < endTime) {
         printf("Question %d:\n", currentQuestion + 1);
         displayQuestion(questions[currentQuestion]);
-        int choice = getUserChoice();
 
-        if (choice == questions[currentQuestion].correct_option) {
-            user.marks++;
+        int choice;
+        if (time(NULL) <= endTime)
+        {
+            choice = getUserChoice();
         }
 
+        if (time(NULL) <= endTime)
+        {
+            if (choice == questions[currentQuestion].correct_option)
+            {
+                user.marks++;
+            }
+        
+        }
         currentQuestion++; // Move to the next question
 
-        if (time(NULL) >= endTime) {
+        if (time(NULL) >= endTime)
+        {
             printf("\nTime's up! Test closed.\n");
             break;
         }
     }
 
-    int elapsedTime = time(NULL) - startTime;
+    int elapsedTime = time(NULL) - startTime;  // time taken to complete the test
 
     FILE *marksFile = fopen("marks.txt", "a");
     if (marksFile == NULL) {
-        printf("Error opening file.\n");
+        printf("Error!! Your marks could not be recorded.\n");
         return 1;
     }
 
     fprintf(marksFile, "Name: %s\nRoll Number: %d\nMarks: %d\nTime taken: %d seconds\n\n",
-            user.name, user.roll_number, user.marks, elapsedTime);
+            user.name, user.roll_number, user.marks, elapsedTime);  // saving the marks in text file
 
     fclose(marksFile);
 
